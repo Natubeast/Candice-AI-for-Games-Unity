f7a9483848894166c11cd8ee5282145e249327d0
Natubeast <54777896+Natubeast@users.noreply.github.com>
2022-01-18 18:04:46 +0200

    Initial Commit

---
+++ Candice AI for Games/Assets/Candice AI for Games/Scripts/Modules/CandiceModuleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CandiceAIforGames.AI
+{
+    public class CandiceModuleMovement: CandiceBaseModule
+    {
+        public CandiceModuleMovement(string moduleName = "CandiceModuleMovement") : base(moduleName) { }
+        public void MoveForward(Transform transform, CandiceAIController aiController)
+        {
+            transform.position += transform.forward * aiController.MoveSpeed * Time.deltaTime;
+        }
+
+        public void LookAt(Transform transform, CandiceAIController aiController)
+        {
+            transform.LookAt(aiController.LookPoint);
+        }
+        public void LookAway(Transform transform, CandiceAIController aiController)
+        {
+            transform.LookAt(-aiController.MainTarget.transform.forward);
+        }
+    }
+}
+
